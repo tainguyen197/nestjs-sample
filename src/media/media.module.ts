035f426d6468e0d3e831f79be1a034bcf919fce2
Nguyen Trung Tai <tainguyen197.ntt@gmail.com>
2025-09-04 14:29:51 +0700

    Add UploadImageController to media module for handling image uploads. Update media.module.ts to include the new controller.

--- src/media/media.module.ts
+++ src/media/media.module.ts
@@ -3,10 +3,11 @@ import { MediaController } from './media.controller';
 import { MediaService } from './media.service';
 import { PrismaModule } from '../prisma/prisma.module';
 import { CommonModule } from '../common/common.module';
+import { UploadImageController } from './upload-image.controller';
 
 @Module({
   imports: [PrismaModule, CommonModule],
-  controllers: [MediaController],
+  controllers: [MediaController, UploadImageController],
   providers: [MediaService],
   exports: [MediaService],
 })
